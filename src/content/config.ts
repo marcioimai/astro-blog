97d1a7de7d846b026b4eff82e2a6c73b919c80a7
Imai <83169891+marcioimai@users.noreply.github.com>
2024-01-24 09:59:59 -0400

    fix: homepage collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const homepage = defineCollection({
-  type: 'content',
-  // Type-check frontmatter using a schema
+  type: 'data',
   schema: z.object({
     title: z.string(),
   }),
@@ -10,11 +9,9 @@ const homepage = defineCollection({
 
 const blog = defineCollection({
   type: 'content',
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
